c1d4105e66b443406b2bdc4ae685f57a3d75e564
Towhidkarim <towhidkarim123@gmail.com>
2025-06-20 23:45:13 +0600

    Add certain configuration for vercel

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { connectDB } from './db/db';
 
 const PORT = process.env.PORT || 3000;
 
-async function main() {
+export default async function main() {
   await connectDB();
   app.listen(PORT, () => console.log(`App Running on Port ${PORT}`));
 }
